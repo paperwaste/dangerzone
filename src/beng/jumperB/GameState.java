8018981bb5e82a85c76cefac5e679360f6310220
Ben Gray <gray.ben.m@gmail.com>
2013-10-02 16:15:15 +1000

    Slight correction to get/set methods in GameState.

--- src/beng/jumperB/GameState.java
+++ src/beng/jumperB/GameState.java
@@ -20,11 +20,11 @@ public class GameState {
 		return size;
 	}
 	public Cell getCell(int x, int y){
-		int i = this.size * y + x % this.size;
+		int i = this.size * y + x;
 		return board[i];
 	}
 	public void setCell(int x, int y, Cell newCell){
-		int i = this.size * y + x % this.size;
+		int i = this.size * y + x;
 		board[i] = newCell;
 	}
 }
